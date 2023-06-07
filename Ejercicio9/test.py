6daef245025e7da1701390bd872f7e9aad164511
maurici8ntivero <123362540+maurici8ntivero@users.noreply.github.com>
2023-06-07 07:30:26 -0300

    Add files via upload

---
+++ Ejercicio9/test.py
@@ -0,0 +1,33 @@
+#El test.......
+import unittest
+from claseVehiculo import Vehiculo
+from claseListaVehiculos import ListaVehiculos
+
+class TestVehiculos(unittest.TestCase):
+    def setUp(self):
+        self.lista_vehiculos = ListaVehiculos()
+        self.vehiculo1 = Vehiculo("Marca1", "Modelo1", 5000, 30000)
+        self.vehiculo2 = Vehiculo("Marca2", "Modelo2", 7000, 35000)
+
+    def test_agregar_vehiculo(self):
+        self.lista_vehiculos.agregar(self.vehiculo1)
+        self.assertEqual(self.lista_vehiculos.mostrar_informacion(0), self.vehiculo1)
+
+    def test_insertar_vehiculo(self):
+       self.lista_vehiculos.agregar(self.vehiculo1)
+       self.lista_vehiculos.insertar(1, self.vehiculo2)
+       self.assertEqual(self.lista_vehiculos.mostrar_informacion(1), self.vehiculo2)
+
+    def test_obtener_vehiculo(self):
+        self.lista_vehiculos.agregar(self.vehiculo1)
+        vehiculo_obtenido = self.lista_vehiculos.mostrar_informacion(0)
+        self.assertEqual(vehiculo_obtenido, self.vehiculo1)
+
+    def test_modificar_precio_base(self):
+        self.lista_vehiculos.agregar(self.vehiculo1)
+        nuevo_precio = 12000
+        self.vehiculo1.set_precio_base(nuevo_precio)
+        self.assertEqual(self.vehiculo1.get_precio_base(), nuevo_precio)
+
+if __name__ == '__main__':
+    unittest.main()
